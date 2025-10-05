d5b6176c001525ad8f2b4aa5915e33f05749f6bb
tasin <tasnim454247734@gmail.com>
2025-10-05 19:37:23 +0600

    Fixed layout issue and added new image

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,10 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-              'https://t4.ftcdn.net/jpg/02/73/55/61/360_F_273556172_SHqUAZGCrhkdf8xFpoPBYKKpWllmBxwU.jpg',
-            ),
+            // image: NetworkImage(
+            //   'https://t4.ftcdn.net/jpg/02/73/55/61/360_F_273556172_SHqUAZGCrhkdf8xFpoPBYKKpWllmBxwU.jpg',
+            // ), takes images from net
+            image: AssetImage('images/diamond.png'), // takes from desktop
           ),
         ),
       ),
